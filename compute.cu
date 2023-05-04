4ce33845dedf55e05020fe2bea51a2b75fc78284
Jingqing Liu <jingqing@udel.edu>
2023-05-03 22:15:59 -0400

    create function sum_and_update_velocity_and_position

--- compute.cu
+++ compute.cu
@@ -26,6 +26,29 @@ __global__ void compute_Pairwise_Accelerations(vector3* hPos, double* mass, vect
 		}
 	} 
 }
+
+//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+__global__ void sum_and_update_velocity_and_position(vector3* hPos, vector3* hVel, vector3* accels, int numEntities, double interval) {
+
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j;
+
+	if (i < numEntities) {
+		vector3 accel_sum={0, 0, 0};
+		for (j = 0; j < NUMENTITIES; j++){
+			for (k = 0;k < 3; k++)
+				accel_sum[k] += accels[i][j][k];
+		}
+	}
+	//compute the new velocity based on the acceleration and time interval
+	//compute the new position based on the velocity and time interval
+	for (k = 0; k < 3; k++){
+		hVel[i][k] += accel_sum[k] * INTERVAL;
+		hPos[i][k] = hVel[i][k] * INTERVAL;
+	}
+}
+
+
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
 //Returns: None
@@ -38,20 +61,7 @@ void compute(){
 	for (i=0;i<NUMENTITIES;i++)
 		accels[i]=&values[i*NUMENTITIES];
 
-	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]=hVel[i][k]*INTERVAL;
-		}
-	}
+	
 	free(accels);
 	free(values);
 }
