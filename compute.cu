5b78e3f9eaec40824787b18abc59f56707e8c1ab
Jingqing Liu <jingqing@udel.edu>
2023-05-10 16:16:51 -0400

    update without seprate

--- compute.cu
+++ compute.cu
@@ -89,7 +89,7 @@ void compute(){
 	cudaMalloc((void**)&device_hVel, sizeof(vector3)*NUMENTITIES);
 	cudaMalloc((void**)&device_mass, sizeof(double)*NUMENTITIES);
 	cudaMalloc((void**)&device_accels, sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	cudaMalloc((void**)&device_accel_sum, sizeof(vector3)*NUMENTITIES);
+	//cudaMalloc((void**)&device_accel_sum, sizeof(vector3)*NUMENTITIES);
 
 	cudaMemcpy(device_hPos, hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyHostToDevice);
 	cudaMemcpy(device_hVel, hVel, sizeof(vector3)*NUMENTITIES, cudaMemcpyHostToDevice);
